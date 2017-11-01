d30cb0aa24334bc4d6103130bc6d8109d83c3478
xioawuxioawu <13168792253@163.com>
2017-11-01 16:39:02 +0800

    下拉刷新

---
+++ app/src/main/java/com/example/rjs/myapplication/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.example.rjs.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by RJS on 2017/11/1.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/example/rjs/myapplication/MainActivity.java
+++ app/src/main/java/com/example/rjs/myapplication/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.rjs.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tip)
+    TextView tip;
+    @BindView(R.id.sr_layout)
+    SwipeRefreshLayout srLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        srLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+//                srLayout.setRefreshing(true);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        srLayout.setRefreshing(false);
+                    }
+                }, 1000);
+            }
+        });
+    }
+
+    @OnClick({R.id.tip, R.id.sr_layout})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tip:
+                break;
+            case R.id.sr_layout:
+                break;
+            default:
+                break;
+        }
     }
 }
